7aee3967283a4c98f63770644ca6624b1e8952d2
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-06 13:56:28 -0700

    changes to get landing page working May6

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+   
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'static_pages#landing_page'
-   
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
