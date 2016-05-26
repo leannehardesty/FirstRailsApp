90ca4fa4a6f46d5127874315e82066a68980b4bc
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-26 14:02:30 -0700

    email server May26

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,16 @@ class StaticPagesController < ApplicationController
      # @products = Product.limit(3) 
       @products = Product.all
    end
+
+   def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		ActionMailer::Base.mail(:from => @email,
+      	:to => 'lhardesty@hotmail.com',
+     	:subject => "A new contact form message from #{@name}",
+      	:body => @message).deliver_now
+	end
    
 
 end
