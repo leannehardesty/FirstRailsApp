0bfb9da39764fc703a142950fac6e8be635854bb
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-26 14:32:21 -0700

    email server setup May26

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,14 +7,21 @@ class StaticPagesController < ApplicationController
       @products = Product.all
    end
 
-   def thank_you
+   # def thank_you
+  		#@name = params[:name]
+  		#@email = params[:email]
+  		#@message = params[:message]
+  		#ActionMailer::Base.mail(:from => @email,
+      	#:to => 'lhardesty@hotmail.com',
+     	#:subject => "A new contact form message from #{@name}",
+      	#:body => @message).deliver_now
+	#end
+
+	def thank_you
   		@name = params[:name]
   		@email = params[:email]
   		@message = params[:message]
-  		ActionMailer::Base.mail(:from => @email,
-      	:to => 'lhardesty@hotmail.com',
-     	:subject => "A new contact form message from #{@name}",
-      	:body => @message).deliver_now
+  		UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
    
 
