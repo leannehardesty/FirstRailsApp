499bb760417397c631d685224d3802f54be0227c
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-30 08:41:00 -0700

    email fixes May30

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,9 @@ class UserMailer < ApplicationMailer
   	def contact_form(email, name, message)
   		@message = message
     	mail(:from => email,
-       	 :to => 'your-email@example.com',
+       	 :to => 'lhardesty@hotmail.com',
        	 :subject => "A new contact form message from #{name}")
   	end
-
-
-
 
 
 end
