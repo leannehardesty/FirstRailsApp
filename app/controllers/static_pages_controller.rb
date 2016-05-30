b6c92adcfb3970bb14f3d0420043a82e0310aae8
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-30 09:50:45 -0700

    email fixes may30

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,22 +7,22 @@ class StaticPagesController < ApplicationController
       @products = Product.all
    end
 
-    def thank_you
-  		@name = params[:name]
-  		@email = params[:email]
-  		@message = params[:message]
-  		ActionMailer::Base.mail(:from => @email,
-      	:to => 'lhardesty@hotmail.com',
-     	  :subject => "A new contact form message from #{@name}",
-      	:body => @message).deliver_now
-	   end
-
-	#def thank_you
+    #def thank_you
   		#@name = params[:name]
   		#@email = params[:email]
   		#@message = params[:message]
-  		#UserMailer.contact_form(@email, @name, @message).deliver_now
-	#end
+  		#ActionMailer::Base.mail(:from => @email,
+      	#:to => 'lhardesty@hotmail.com',
+     	  #:subject => "A new contact form message from #{@name}",
+      	#:body => @message).deliver_now
+	   #end
+
+	def thank_you
+  		@name = params[:name]
+  		@email = params[:email]
+  		@message = params[:message]
+  		UserMailer.contact_form(@email, @name, @message).deliver_now
+	end
    
 
 end
