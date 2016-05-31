b46d1273aab259aa4d1809142f8b9bd7bdbdca34
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-31 10:23:54 -0700

    authorization fixes May31

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
     
 
-#rescue_from CanCan::AccessDenied do |exception|
-  #redirect_to main_app.root_url, :alert => exception.message
-#end
+	rescue_from CanCan::AccessDenied do |exception|
+	  redirect_to main_app.root_url, :alert => exception.message
+	end
 
   
 end
