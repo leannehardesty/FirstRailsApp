e7e5f86a24f5b40a0cf7c9f6f5ab4898d49f9d8b
Leanne Hardesty <lhardesty@hotmail.com>
2016-05-31 12:44:15 -0700

    authorization fixes May31

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-    
 
-<<<<<<< HEAD
-	rescue_from CanCan::AccessDenied do |exception|
-	  redirect_to main_app.root_url, :alert => exception.message
-	end
-
-=======
 	#customize message if a user tries to edit another user for whom they are not authorized
+
 	rescue_from CanCan::AccessDenied do |exception|
 	  redirect_to main_app.root_url, :alert => exception.message
 	end
->>>>>>> master
   
 end
