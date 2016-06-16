2ae1262e5df0b4190f25077a56390b08ba383b85
Leanne Hardesty <lhardesty@hotmail.com>
2016-06-15 20:09:52 -0700

    fixes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,7 @@ class ProductsController < ApplicationController
        #@products = Product.where("name ilike ?", "%#{search_term}%")
        @products = Product.where("name = ?", search_term)
        #@products = Product.where("name LIKE ?", "%#{search_term}%") 
+       
     else
        @products = Product.all
     end
