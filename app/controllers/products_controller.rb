772e58de30cf052868523332e2347742c89fd9fa
Leanne Hardesty <lhardesty@hotmail.com>
2016-06-15 20:02:32 -0700

    search fix June15

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
     if params[:q]
        search_term = params[:q]
        @products = Product.where("name ilike ?", "%#{search_term}%")
-        
+       #@products = Product.where("name LIKE ?", "%#{search_term}%") 
     else
        @products = Product.all
     end
