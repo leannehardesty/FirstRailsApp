dbba1c9f6f5e1c8a2076078ac5f35b0d750af933
Leanne Hardesty <lhardesty@hotmail.com>
2016-06-18 11:59:49 -0700

    fixes for Stripe June18

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,13 +2,13 @@
 
 if Rails.env.production?
   Rails.configuration.stripe = {
-    :publishable_key => ENV['pk_live_uCV4XWu7jJbvXsCyZn2Bc3Wx'],
-    :secret_key => ENV['sk_live_XCI6RMqkR3sms2FLTrYteaZX']
+   :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
   }
 else
   Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_Ib8qFAMOjXgNjZ2yCs4N4Qu7',
-    :secret_key => 'sk_test_5xghPRjZueXaZprsEPIEbtm8'
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
   }
 end
 
