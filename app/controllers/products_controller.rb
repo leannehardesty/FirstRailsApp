c31fbc010788a298f79bc3e02410d099f2a64874
Leanne Hardesty <lhardesty@hotmail.com>
2016-06-19 21:29:28 -0700

    fixes search june19

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,15 +32,6 @@ class ProductsController < ApplicationController
   end
      
    
-collection = [2, 7, 9]
-
-puts "iterating over a collection of #{collection.count} items"
-collection.each do |item|
-  puts "Item before increment: #{item}"
-  item.increment!
-  puts "Item after increment #{item}"
-end
-puts "done iterating!"
 
   
 
