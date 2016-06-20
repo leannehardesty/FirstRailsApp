dc9800dd1786bef5ed0d852b5cd21199140ea129
Leanne Hardesty <lhardesty@hotmail.com>
2016-06-19 20:50:21 -0700

    search fix June19

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,8 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-          @products = Product.where("name ilike ?", "%#{search_term}%")
-          #@products = Product.where("lower(name) like ?", "%#{params[:q].downcase}%")
+          #@products = Product.where("name ilike ?", "%#{search_term}%")
+          @products = Product.where("lower(name) like ?", "%#{params[:q].downcase}%")
 
         #if Rails.env.development?
           #@products = Product.where("name LIKE ?", "%#{search_term}%")
